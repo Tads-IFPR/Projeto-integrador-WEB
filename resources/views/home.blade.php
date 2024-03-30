359d42522ebb0966a1e5bb0c261f3323a02e3c9d
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2024-03-30 14:20:19 -0300

    enhancement: home audio cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,13 +12,13 @@
             </div>
         </div>
         <div class="d-flex flex-column" id="audios">
-            <div class="d-flex justify-content-between w-100 mb-3">
+            <div class="d-flex justify-content-between w-100">
                 <h2 class="cfs-1 bolder">Musics</h2>
                 <a href="{{route('audio.create')}}" class="button-default px-4 py-1">New</a>
             </div>
             <div class="d-flex w-100 wrap">
                 @foreach ($audios as $audio)
-                    <livewire:audio-card :$audio :key="$audio->id"/>
+                    <livewire:audio-card :$audio :key="$audio->id" class="mt-3"/>
                 @endforeach
             </div>
         </div>
@@ -52,10 +52,6 @@
         .audio-card {
             width: 100%;
             min-width: 100%;
-        }
-
-        .audio-card:not(:first-child) {
-            margin-top: 1rem;
         }
     }
 </style>
