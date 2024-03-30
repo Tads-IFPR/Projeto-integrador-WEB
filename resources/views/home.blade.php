f6e633d7b6d8dd30b35edeccb6cd8d8b041714a3
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2024-03-30 02:04:52 -0300

    fix: home music title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,14 +6,14 @@
         <div class="d-flex flex-column" id="playlists">
             <div class="col-12">
                 <div class="d-flex justify-content-between">
-                    <h2 class="cfs-2 bolder">Playlists</h2>
+                    <h2 class="cfs-1 bolder">Playlists</h2>
                     <button class="button-default px-4 py-1">New</button>
                 </div>
             </div>
         </div>
         <div class="d-flex flex-column" id="audios">
             <div class="d-flex justify-content-between w-100 mb-3">
-                <h2 class="cfs-2 bolder">Audios</h2>
+                <h2 class="cfs-1 bolder">Musics</h2>
                 <a href="{{route('audio.create')}}" class="button-default px-4 py-1">New</a>
             </div>
             <div class="d-flex w-100 wrap">
