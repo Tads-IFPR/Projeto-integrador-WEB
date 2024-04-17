0fe4823443299d309baaf32ce0e1a830973f55cc
JacksonChaves <jackson10thompson@gmail.com>
2024-04-17 20:49:26 -0300

    wip navbar

---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,27 @@
+@import('css/navbar.css')
+
+
+<div>
+    <div></div>
+        <nav>
+            <div class="buttons">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Community</a></li>
+                    <li><a href="#">Favorites</a></li>
+                </ul>
+            </div>
+            <div class="search"
+                <input type="text" placeholder="Search here">
+            </div>
+            <div class="configuration">
+                <button>Configuration</button>
+            </div>
+        </nav>
+
+    </div>
+</div>
+<div>
+
+</div>
+
