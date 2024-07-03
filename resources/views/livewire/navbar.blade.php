40e99a9c09463c51972a9fb0fffe5801334ceff1
JacksonChaves <jackson10thompson@gmail.com>
2024-07-03 19:38:21 -0300

    atualizacao navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-
-    <link rel="stylesheet" href="style2.css">
-
-
-</head>
-<body>
-
 
     <nav class="navbar navbar-expand-md navbar-dark mb-4" id="main-nav">
         <div class="container-fluid">
@@ -57,7 +41,5 @@
           </div>
         </div>
     </nav>
-</body>
-</html>
 
 
