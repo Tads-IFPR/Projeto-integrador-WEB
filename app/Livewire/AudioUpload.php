35ef1bac8fd4f6e87af57731d88f1d370bd4d1cf
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2024-07-29 00:06:08 -0300

    feat(audio upload/php): uploading single audio

--- app/Livewire/AudioUpload.php
+++ app/Livewire/AudioUpload.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Http\Repositories\AudioRepository;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -9,25 +10,27 @@ class AudioUpload extends Component
 {
     use WithFileUploads;
 
-    public $file;
-
     protected $rules = [
         'file' => 'required|file|max:10000|mimes:audio/mpeg,mpga,mp3,wav,aac',
     ];
 
-    public function upload()
+    public $file;
+
+    public function updatedFile()
     {
         $this->validate();
+        $this->dispatch('fileProcessed');
+    }
 
-        $path = $this->file->store('audios');
-
-        // Aqui você pode salvar o caminho no banco de dados ou realizar outras ações
-
-        session()->flash('message', 'Upload bem-sucedido!');
+    public function save()
+    {
+        AudioRepository::store($this->file);
+        $this->redirect('/');
+        session()->flash('message', 'File successfully uploaded.');
     }
 
     public function render()
     {
-        return view('livewire.audio-upload');
+        return view('livewire.audio.audio-upload');
     }
 }
