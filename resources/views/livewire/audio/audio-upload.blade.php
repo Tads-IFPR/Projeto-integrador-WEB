8066f6a696726fd7b7751d98490573c9015d9f57
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2024-07-29 22:15:18 -0300

    enhancement(audio/upload): show custom overlay

--- resources/views/livewire/audio/audio-upload.blade.php
+++ resources/views/livewire/audio/audio-upload.blade.php
@@ -1,4 +1,12 @@
 <div>
+    <div wire:loading.class="show" wire:target="files" id="uploading">
+        <div class="loader"></div>
+        <h1>Uploading...</h1>
+    </div>
+    <div id="drop-overlay">
+        <h1>Drag files to upload</h1>
+        <img src="/imgs/green-arrow.png" alt="A green arrow pointed to top" id="arrow-upload">
+    </div>
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -25,7 +33,7 @@
     document.addEventListener('DOMContentLoaded', function() {
         const dropArea = document.body;
         const fileInput = document.getElementById('fileElem');
-        let overlay;
+        var overlay = document.getElementById('drop-overlay');
 
         ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
             dropArea.addEventListener(eventName, preventDefaults, false);
@@ -47,32 +55,16 @@
         dropArea.addEventListener('drop', handleDrop, false);
 
         function showOverlay(e) {
-            if (!overlay) {
-                overlay = document.createElement('div');
-                overlay.id = 'drop-overlay';
-                overlay.style.position = 'fixed';
-                overlay.style.top = 0;
-                overlay.style.left = 0;
-                overlay.style.width = '100vw';
-                overlay.style.height = '100vh';
-                overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-                overlay.style.zIndex = 1000;
-                overlay.style.display = 'flex';
-                overlay.style.justifyContent = 'center';
-                overlay.style.alignItems = 'center';
-                overlay.style.color = 'white';
-                overlay.style.fontSize = '24px';
-                overlay.innerText = 'Drop the files here';
-                document.body.appendChild(overlay);
+            if (!overlay.classList.contains('add')) {
+                overlay.classList.add('show');
+                document.getElementsByTagName('body')[0].classList.add('remove-scroll');
                 overlay.addEventListener('dragleave', hideOverlay, false);
             }
         }
 
         function hideOverlay(e) {
-            if (overlay) {
-                document.body.removeChild(overlay);
-                overlay = null;
-            }
+            overlay.classList.remove('show')
+            document.getElementsByTagName('body')[0].classList.remove('remove-scroll');
         }
 
         function handleDrop(e) {
@@ -94,3 +86,80 @@
         });
     });
 </script>
+
+@push('styles')
+<style>
+    #drop-overlay,
+    #uploading {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        display: none;
+        font-size: 24px;
+    }
+
+    #uploading h1 {
+        color: var(--white);
+        font-weight: bolder;
+        margin-top: .5rem;
+    }
+
+    #drop-overlay {
+        outline:.5rem dashed var(--primary);
+        outline-offset:-1.5rem;
+    }
+
+    #drop-overlay h1 {
+        color: var(--white);
+        background-color: var(--secondary);
+        border-radius: .5rem;
+        font-weight: bolder;
+        padding: 1rem 3rem;
+    }
+
+    #drop-overlay.show,
+    #uploading.show {
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 1000;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+    }
+
+    #arrow-upload {
+        animation: bounce 2s infinite;
+        margin-top: .7rem;
+    }
+
+    .remove-scroll {
+        overflow: hidden;
+    }
+
+    @keyframes bounce {
+        0%, 100% {
+            transform: translateY(0);
+        }
+        50% {
+            transform: translateY(2rem);
+        }
+    }
+
+    .loader {
+        border: 16px solid var(--primary);
+        border-top: 16px solid transparent;
+        border-radius: 50%;
+        animation: spin 2s linear infinite;
+        width: 7rem;
+        height: 7rem;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
+@endpush
