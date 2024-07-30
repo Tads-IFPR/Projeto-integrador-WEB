95cd2422ed40342fdc2ef39271f91d0a4f2a068f
AntonioCarlos850 <antoniocarlosjr850@gmail.com>
2024-07-30 00:03:10 -0300

    fix(audio/card): now on click in audio this start play

--- resources/views/livewire/audio/audio-card.blade.php
+++ resources/views/livewire/audio/audio-card.blade.php
@@ -1,9 +1,10 @@
 <div class="audio-card d-flex justify-content-between px-1 {{$class}}"
     style="font-variation-settings: 'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48;"
+    wire:click='play'
 >
     <div class="d-flex justify-content-center align-items-center" style="min-width: 50px">
         <img width="50px" src="{{ route('audio.show.image', $audio) }}" class="h-100" alt="Audio cover image">
-        <button wire:click='play' class="play">
+        <button class="play">
             <span class="material-symbols-outlined">play_arrow</span>
         </button>
     </div>
