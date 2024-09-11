b5eec1443997422810f3334ff23f96d5badc7301
JacksonChaves <jackson10thompson@gmail.com>
2024-09-11 20:48:56 -0300

    Correção conflitos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserUpdateRequest;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Request;
 
 
 class UserController extends Controller
 {
+
+    public function edit(User $user)
+    {
+        return view('user.edit', [
+            'user' => $user
+        ]);
+    }
+
+    public function update(UserUpdateRequest $request)
+    {
+        $validated = $request->validated();
+
+        auth()->user()->fill([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+        ]);
+
+        if ($validated['password']) {
+            auth()->user()->password = Hash::make($validated['password']);
+        }
+
+        auth()->user()->save();
+
+        return redirect()->route('home');
+    }
     public function getAll(Request $request)
     {
 
