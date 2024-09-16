735bcc962e748052913002da3f3a3a0e670bc1be
LRJonas <jonas.robertinho@gmail.com>
2024-09-16 16:25:51 -0300

    ajustei o idioma da página de edição para o inglês

--- resources/views/playlist/edit.blade.php
+++ resources/views/playlist/edit.blade.php
@@ -2,33 +2,33 @@
 
 @section('content')
 
-    <h1>Crie uma Playlist</h1>
+    <h1>Create a Playlist</h1>
     <form action="{{ route('playlist.update', $playlist->id) }}" method="POST" enctype="multipart/form-data">
         @method('PUT')
         @csrf
         <x-input type="text" name="name" placeholder="Name" class="w-100" value="{{$playlist->name}}"/>
 
         <div>
-            <label for="is_public">Pública</label>
+            <label for="is_public">Public</label>
             <input type="checkbox" class="form-control" id="is_public" name="is_public" value="1" {{$playlist->is_public ? 'checked' : ''}}>
         </div>
         <div>
-            <label for="cover_path">Imagem da Playlist</label>
+            <label for="cover_path">Playlist Image</label>
         <!--<input type="file" class="form-control" id="cover_path" name="cover_path"> -->
             <x-input type="file" name="cover_path" id="cover" accept="image/*" class="py-2 w-100" />
         </div>
        
         <!--<x-button type="submit" id="submit" class="px-4 py-1 bolder mt-3">Create</x-button>-->
 
-        <button type="submit" class="btn btn-primary">Salvar</button>
+        <button type="submit" class="button-default px-4 py-1" id="edit-save">Save</button>
             
         
     </form>
 
 
 
-<div>
-    <a href="{{ route('home') }}">Voltar</a>
+<div >
+    <a href="{{ route('home') }}" class="button-default px-4 py 1">Back to Home</a>
 </div>
 
 <style>
